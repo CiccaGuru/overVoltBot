0087954629454b957208c6cbc9bed2cd30f9bff7
CiccaGuru <robyciccarelli.rc@gmail.com>
2017-09-12 12:13:54 +0200

    Random video

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from telepot.aio.delegate import per_inline_from_id, create_open, pave_event_spa
 from uuid import uuid4
 from apiclient.discovery import build
 from apiclient.errors import HttpError
+from oauth2client.tools import argparser
+from random import randint
 
 def p(text):
     print(text)
@@ -25,6 +27,7 @@ class OverVoltBot(InlineUserHandler, AnswererMixin):
         newUrl = url
         messaggio = "</i>Impossibile applicare il referral</i>"
         success = False
+        store = ""
         if "amazon.it" in url:
             length = len(url);
             tagIndex = url.find("tag=")
@@ -50,19 +53,24 @@ class OverVoltBot(InlineUserHandler, AnswererMixin):
             store = "Amazon"
         return (success, messaggio, store)
 
-    def searchYoutube(self, query, numResults):
+    def searchYoutube(self, query, numResults, random):
         youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
         developerKey=DEVELOPER_KEY)
         results = []
-        search_response = youtube.search().list(
-            q=query,
-            part="id,snippet",
-            maxResults=numResults,
-            order="viewCount",
-            type="video",
-            channelId="UCw6ekhAtFahKr7gImCIoYwg"
-        ).execute()
-        for search_result in search_response.get("items", []):
+        p(random)
+        if(random):
+            search_response = youtube.search().list(
+                q="",
+                part="id,snippet",
+                maxResults=20,
+                order="viewCount",
+                type="video",
+                channelId="UCw6ekhAtFahKr7gImCIoYwg"
+            ).execute()
+            items = search_response.get("items", [])
+            p(items)
+            rand = randint(0, len(items)-1)
+            search_result = items[rand]
             id_articolo = uuid4().hex
             if search_result["id"]["kind"] == "youtube#video":
                 results.append({'type': 'video',
@@ -73,6 +81,26 @@ class OverVoltBot(InlineUserHandler, AnswererMixin):
                                  'description': search_result["snippet"]["description"],
                                  "thumb_url":   search_result["snippet"]["thumbnails"]["default"]["url"],
                                  "message_text": "www.youtube.it/watch?v=%s" % search_result["id"]["videoId"]})
+        else:
+            search_response = youtube.search().list(
+                q=query,
+                part="id,snippet",
+                maxResults=numResults,
+                order="relevance",
+                type="video",
+                channelId="UCw6ekhAtFahKr7gImCIoYwg"
+            ).execute()
+            for search_result in search_response.get("items", []):
+                id_articolo = uuid4().hex
+                if search_result["id"]["kind"] == "youtube#video":
+                    results.append({'type': 'video',
+                                     'id': id_articolo,
+                                     'video_url': "www.youtube.it/watch?v=%s" % search_result["id"]["videoId"],
+                                     "mime_type": "text/html",
+                                     'title': search_result["snippet"]["title"],
+                                     'description': search_result["snippet"]["description"],
+                                     "thumb_url":   search_result["snippet"]["thumbnails"]["default"]["url"],
+                                     "message_text": "www.youtube.it/watch?v=%s" % search_result["id"]["videoId"]})
         return results
                         
     def on_inline_query(self, msg):
@@ -94,23 +122,23 @@ class OverVoltBot(InlineUserHandler, AnswererMixin):
         p(str(self.id) + ':' + 'Chosen Inline Result:' + " "+ str(result_id) + " "+ str(from_id) + " "+ str(query_string))
 
     async def on_chat_message(self, msg):
-        p(msg["text"])
         id_referral = uuid4().hex
         splitted = msg["text"].split()
         if splitted[0] == "/referral":
-            url = " ".join(splitted[1:])
-            (status,messaggio,store) = self.getReferralLink(url)
+            (status,messaggio,store) = self.getReferralLink(" ".join(splitted[1:]))
             await self.sender.sendMessage(messaggio)
         elif splitted[0] == "/youtube":
-            results = self.searchYoutube(splitted[1:], 1)
-            messaggio = results[0]["message_text"]
-            await self.sender.sendMessage(messaggio)
+            results = self.searchYoutube(" ".join(splitted[1:]), 5, (len(splitted)<=1))
+            if len(results)>0:
+                messaggio = results[0]["message_text"]
+            else:
+                messaggio = "<i>Nessun risultato</i>"
+            await self.sender.sendMessage(messaggio, parse_mode = "html")
         elif splitted[0] == "/start":
             await self.sender.sendMessage("Comandi disponibili: \n - /referral [link]: applica il referral di OverVolt \n - /youtube [query]: ricerca tra i video di OverVolt", parse_mode = "html")
 
-
 #TOKEN = "420659811:AAFF2rKdrUXxXuHQW0KPZt8SUxwRf-CRBE8"   ##PRODUCTION
-TOKEN = "371830775:AAEZld4C0qyuvxStk10ojImvBoKo5CNDsYY"  ##TEST
+#TOKEN = "371830775:AAEZld4C0qyuvxStk10ojImvBoKo5CNDsYY"  ##TEST
 TOKEN = "235898396:AAHCcT94w-aCZS2THya8ho2SIc2xLDvVkQ0" #MARCO
 bot = telepot.aio.DelegatorBot(TOKEN, [
     pave_event_space()(
