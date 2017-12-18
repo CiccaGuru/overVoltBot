58ec90ef6607972472f8a5ffa0bd95a9a7cc0526
CiccaGuru <robyciccarelli.rc@gmail.com>
2017-12-18 23:23:18 +0100

    Clean up for GitHub

--- main.py
+++ main.py
@@ -9,55 +9,72 @@ from apiclient.discovery import build
 from apiclient.errors import HttpError
 from oauth2client.tools import argparser
 from random import randint
+import emoji
 
-def p(text):
-    print(text)
-    sys.stdout.flush()
-
-DEVELOPER_KEY = "AIzaSyDauAadm_7YeM9vW_8LSFm9KnUydhRynqY"
-YOUTUBE_API_SERVICE_NAME = "youtube"
-YOUTUBE_API_VERSION = "v3"
-CHANNEL_ID = ""
 class OverVoltBot(InlineUserHandler, AnswererMixin):
     def __init__(self, *args, **kwargs):
         super(OverVoltBot, self).__init__(*args, **kwargs)
         self.count = 0
+        read_dev_key = open('DEVELOPER_KEY')
+        self.DEVELOPER_KEY = read_dev_key.read().strip()
+        read_dev_key.close()
+        self.YOUTUBE_API_SERVICE_NAME = "youtube"
+        self.YOUTUBE_API_VERSION = "v3"
+        read_channel_id = open("CHANNEL_ID")
+        self.CHANNEL_ID = read_channel_id.read().strip()
+        self.GEARBEST_REFERRAL = "12357131"
+        self.BANGGOOD_REFERRAL = "63091629786202015112"
 
     def getReferralLink(self, url):
         newUrl = url
-        messaggio = "</i>Impossibile applicare il referral</i>"
+        messaggio = '<i>Impossibile applicare il referral</i>'
         success = False
         store = ""
         if "amazon.it" in url:
-            length = len(url);
-            tagIndex = url.find("tag=")
-            while tagIndex > 0:
-                nextParameterIndex = url.find("&", tagIndex)
-                if nextParameterIndex+tagIndex+1>=length or nextParameterIndex <0:
-                    url = url[:tagIndex];
-                else:
-                    url = url[:tagIndex] + url.slice[nextParameterIndex + tagIndex:]
-                length = url.length;
-                tagIndex =url.find("tag=")
-            separator = url.find("?")>0 and  "&" or "?"
-            newUrl = url + separator + "tag=overVolt-21"
-            messaggio = newUrl
+            messaggio = '<i>Purtroppo attualmente non funzionano i referral per Amazon</i>'
             success = True
             store = "Amazon"
         elif "banggood.com" in url:
-            index = url.find(".html");
-            if not (".html?p=63091629786202015112" in url) and index > 0:
-                newUrl = url[0:index] +  ".html?p=63091629786202015112"
+            length = len(url)
+            indexHtml = url.find(".html")
+            indexTag = url.find("p=")
+            if indexHtml > 0:
+                while indexTag > 0:
+                    nextParameterIndex = url[indexTag:].find("&")
+                    if (nextParameterIndex+indexTag+1>=length) or (nextParameterIndex <0):
+                        url = url[:(indexTag-1)]
+                    else:
+                        url = url[:(indexTag-1)] + url[(nextParameterIndex+indexTag):]
+                    length = len(url)
+                    indexTag = url.find("p=")
+                separator = url.find("?")>0 and "&" or "?";
+                newUrl = url + separator +  "p="+self.BANGGOOD_REFERRAL;
             messaggio = newUrl
             success = True
-            store = "Amazon"
+            store = "Banggood"
+        elif "gearbest.com" in url:
+            length = len(url)
+            indexHtml = url.find(".html")
+            indexTag = url.find("lkid=")
+            if indexHtml > 0:
+                while indexTag > 0:
+                    nextParameterIndex = url[indexTag:].find("&")
+                    if (nextParameterIndex+indexTag+1>=length) or (nextParameterIndex <0):
+                        url = url[:(indexTag-1)]
+                    else:
+                        url = url[:(indexTag-1)] + url[(nextParameterIndex+indexTag):]
+                    length = len(url)
+                    indexTag = url.find("lkid=")
+                separator = url.find("?")>0 and "&" or "?";
+                newUrl = url + separator +  "lkid="+self.GEARBEST_REFERRAL;
+            messaggio = newUrl
+            success = True
+            store = "Gearbest"
         return (success, messaggio, store)
 
     def searchYoutube(self, query, numResults, random):
-        youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
-        developerKey=DEVELOPER_KEY)
+        youtube = build(self.YOUTUBE_API_SERVICE_NAME, self.YOUTUBE_API_VERSION, developerKey=self.DEVELOPER_KEY)
         results = []
-        p(random)
         if(random):
             search_response = youtube.search().list(
                 q="",
@@ -65,10 +82,9 @@ class OverVoltBot(InlineUserHandler, AnswererMixin):
                 maxResults=20,
                 order="viewCount",
                 type="video",
-                channelId="UCw6ekhAtFahKr7gImCIoYwg"
+                channelId=self.CHANNEL_ID
             ).execute()
             items = search_response.get("items", [])
-            p(items)
             rand = randint(0, len(items)-1)
             search_result = items[rand]
             id_articolo = uuid4().hex
@@ -88,7 +104,7 @@ class OverVoltBot(InlineUserHandler, AnswererMixin):
                 maxResults=numResults,
                 order="relevance",
                 type="video",
-                channelId="UCw6ekhAtFahKr7gImCIoYwg"
+                channelId=self.CHANNEL_ID
             ).execute()
             for search_result in search_response.get("items", []):
                 id_articolo = uuid4().hex
@@ -109,37 +125,39 @@ class OverVoltBot(InlineUserHandler, AnswererMixin):
             id_referral = uuid4().hex
             url = telegramQuery
             articles = []
-            (success, messaggio, store) = getReferralLink(telegramQuery)
+            (success, messaggio, store) = self.getReferralLink(telegramQuery)
             if success:
                 articles.append({'type': 'article', 'id': id_referral, 'title': "Applica referral su "+store, 'message_text': messaggio})
-            articles.extend(self.searchYoutube(telegramQuery, 5))
+            articles.extend(self.searchYoutube(telegramQuery, 5, False))
             return articles
         self.answerer.answer(msg, compute_answer)
 
     def on_chosen_inline_result(self, msg):
-        p("CIAO");
-        result_id, from_id, query_string = telepot.glance(msg, flavor='chosen_inline_result')
-        p(str(self.id) + ':' + 'Chosen Inline Result:' + " "+ str(result_id) + " "+ str(from_id) + " "+ str(query_string))
+        result_id, from_id, query_string = telepot.glance(msg, flavor='chosen_inline_result', parse_mode = "html")
 
     async def on_chat_message(self, msg):
         id_referral = uuid4().hex
         splitted = msg["text"].split()
         if splitted[0] == "/referral":
             (status,messaggio,store) = self.getReferralLink(" ".join(splitted[1:]))
-            await self.sender.sendMessage(messaggio)
+            await self.sender.sendMessage(messaggio, parse_mode = "html")
         elif splitted[0] == "/youtube":
             results = self.searchYoutube(" ".join(splitted[1:]), 5, (len(splitted)<=1))
             if len(results)>0:
                 messaggio = results[0]["message_text"]
             else:
-                messaggio = "<i>Nessun risultato</i>"
+                messaggio = "Nessun risultato"
             await self.sender.sendMessage(messaggio, parse_mode = "html")
         elif splitted[0] == "/start":
             await self.sender.sendMessage("Comandi disponibili: \n - /referral [link]: applica il referral di OverVolt \n - /youtube [query]: ricerca tra i video di OverVolt", parse_mode = "html")
+        elif splitted[0] == "/help":
+            await self.sender.sendMessage("Ciao, sono il bot di overVolt :robot:! \n\n Per ora so: \n\n<b>Aggiungere un referral a un link</b>\nMandami un link di <b>Banggood</b> o <b>Gearbest</b> con il comando /referral &lt link &gt e io aggiungerò il referral! \n\n<b>Cercare su YouTube un video di overVolt</b>\nUsa il comando /youtube &lt stringa &gt per cercare su Youtube!", parse_mode = "html")
+        elif msg['from']['id'] == msg['chat']['id']:
+            await self.sender.sendMessage(emoji.emojize('Non ho capito :pensive_face:\n\n Scrivi /help per sapere come funziono.'), parse_mode = "html")
 
-#TOKEN = "420659811:AAFF2rKdrUXxXuHQW0KPZt8SUxwRf-CRBE8"   ##PRODUCTION
-#TOKEN = "371830775:AAEZld4C0qyuvxStk10ojImvBoKo5CNDsYY"  ##TEST
-TOKEN = "235898396:AAHCcT94w-aCZS2THya8ho2SIc2xLDvVkQ0" #MARCO
+token_file = open('TOKEN_TEST')
+TOKEN = token_file.read().strip()
+token_file.close()
 bot = telepot.aio.DelegatorBot(TOKEN, [
     pave_event_space()(
         per_inline_from_id(), create_open, OverVoltBot, timeout=1),
@@ -149,5 +167,4 @@ bot = telepot.aio.DelegatorBot(TOKEN, [
 loop = asyncio.get_event_loop()
 
 loop.create_task(MessageLoop(bot).run_forever())
-p('Listening ...')
 loop.run_forever()
